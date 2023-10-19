8e74aefad766e3fd7d0cbd5f18d9a53d5ec49c0d
satojkovic <satojkovic@gmail.com>
2023-10-19 23:41:17 +0900

    Rename embed_dim with patch_dim

--- vit_pytorch/vit.py
+++ vit_pytorch/vit.py
@@ -3,15 +3,15 @@ import torch.nn as nn
 
 
 class Patches(nn.Module):
-    def __init__(self, in_channels=3, patch_size=16, embed_dim=192):
+    def __init__(self, in_channels=3, patch_size=16, patch_dim=192):
         super(Patches, self).__init__()
         self.in_channels = in_channels
-        self.embed_dim = embed_dim
+        self.patch_dim = patch_dim
         self.patch_size = patch_size
 
         self.conv = nn.Conv2d(
             in_channels=self.in_channels,
-            out_channels=self.embed_dim,
+            out_channels=self.patch_dim,
             kernel_size=self.patch_size,
             stride=self.patch_size,
         )
