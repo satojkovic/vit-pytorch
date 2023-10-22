e907856002810f3bb6c021375308c7a731c28ee9
satojkovic <satojkovic@gmail.com>
2023-10-22 20:56:04 +0900

    Add PatchEncoder class

--- vit_pytorch/vit.py
+++ vit_pytorch/vit.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import einops
 
 
 class Patches(nn.Module):
@@ -31,8 +32,38 @@ class Patches(nn.Module):
         return patches
 
 
+class PatchEncoder(nn.Module):
+    def __init__(self, num_patches, patch_dim=192, embed_dim=192):
+        super(PatchEncoder, self).__init__()
+        self.patch_dim = patch_dim
+        self.embed_dim = embed_dim
+        self.encoder = nn.Linear(
+            in_features=self.patch_dim, out_features=self.embed_dim
+        )
+        self.cls_token = nn.Parameter(torch.randn(1, 1, self.embed_dim))
+        self.pos_embedding = nn.Parameter(
+            torch.randn(1, num_patches + 1, self.embed_dim)
+        )
+
+    def forward(self, x):
+        batch_size = x.shape[0]
+        x = self.encoder(x)
+        # Add cls token
+        cls_tokens = einops.repeat(self.cls_token, "1 1 d -> b 1 d", b=batch_size)
+        x = torch.cat((cls_tokens, x), dim=1)
+        # Add position embedding
+        x += self.pos_embedding
+
+        return x
+
+
 if __name__ == "__main__":
     patch_extractor = Patches()
     dummy_x = torch.randn((10, 3, 224, 224))
     patches = patch_extractor(dummy_x)
     print(f"patches: {patches.shape}")
+
+    num_patches = patches.shape[1]
+    patch_encoder = PatchEncoder(num_patches=num_patches)
+    encoded_patches = patch_encoder(patches)
+    print(f"encoded_patches: {encoded_patches.shape}")
